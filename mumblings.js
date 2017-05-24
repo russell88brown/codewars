96c5fe31e3174735e89ee73cc32e86699ba7b5f3
russell88brown <russell88brown@gmail.com>
2017-05-25 04:15:53 +0800

    mumblings not caps corrected

---
+++ mumblings.js
@@ -0,0 +1,25 @@
+/*
+https://www.codewars.com/kata/mumbling
+
+accum("abcd");    // "A-Bb-Ccc-Dddd"
+accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt");    // "C-Ww-Aaa-Tttt"
+*/
+
+var newString
+
+function accum(inputString) {
+	newString = ''
+	stringLength = inputString.length;
+	for ( i = 0; i < stringLength ; i++) {
+		if (i != 0) {newString = newString + '-'};
+		for (ii = 0; ii < i+1; ii++) {
+		newString = newString + inputString.substring(i,i+1)
+		}
+	}
+	return newString
+}
+
+console.log(accum("abcd"));
+console.log(accum("RqaEzty"));
+console.log(accum("cwAt"));
