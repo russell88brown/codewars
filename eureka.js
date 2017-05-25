72e0a9564f38c560fd4c20842a440fa7433c814e
russell88brown <russell88brown@gmail.com>
2017-05-25 14:31:40 +0800

    eureka, calculation check

--- eureka.js
+++ eureka.js
@@ -1,27 +1,26 @@
 /* 
 
 https://www.codewars.com/kata/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka
-
-The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
-
-In effect: 89 = 8^1 + 9^2
-
-The next number in having this property is 135.
-
-See this property again: 135 = 1^1 + 3^2 + 5^3
-
-We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
-
-Let's see some cases:
-
-sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
-
+steps: 
 */
 
-function sumDigPow(a, b) {
-  // Your code here
+//calculation to check whether sum of digits (to the power of position) matches original
+
+
+originalNumber = 135
+tempArray = [1,3,5]
+arraySum = 0
+augArray = []
+
+for (i = 0; i < tempArray.length; i++) {
+	arraySum += Math.pow(tempArray[i],i+1);
+	}
+console.log('arraySum: ' + arraySum + ' - ' + 'original number: ' + originalNumber);
+if (arraySum = originalNumber) {
+	augArray.push(originalNumber);
 }
+
+console.log(augArray)
+
 
 
