bfd7c40032c832455a9bf3f667a78626b32e19f3
russell88brown <russell88brown@gmail.com>
2017-05-25 17:18:38 +0800

    added codeacademy answers, issues with **

--- eureka.js
+++ eureka.js
@@ -1,10 +1,69 @@
 /* 
-
 https://www.codewars.com/kata/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka
 steps: 
 */
 
-/*calculation to check whether sum of digits (to the power of position) matches original
+// function solution my answer
+console.log('my answer')
+
+function sumDigPow(a, b) {
+  var originalArray = [];
+	for (var i = a; i < b + 1; i++) {
+	    originalArray.push(i);
+    } 
+    var augArray = [];
+    for (var ii = 0; ii < originalArray.length; ii++) {
+	    var tempArray = originalArray[ii].toString().split("");
+	    var arraySum = 0;
+	    for (var iii = 0; iii < tempArray.length; iii++) {
+		    arraySum = arraySum + Math.pow(tempArray[iii],iii+1);
+		}
+	    if (arraySum == originalArray[ii]) {
+		    augArray.push(originalArray[ii]);
+        }
+    }
+    return augArray
+}
+
+console.log(sumDigPow(1, 10000000));
+
+/* answer one - issue with syntax
+console.log('code wars answer 1')
+
+function sumDigPowAnswerOne(a, b) {
+  var ans = [];
+  while(a <= b){
+    if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
+      ans.push(a);
+    a++;
+  }
+  return ans;
+}
+
+console.log(sumDigPowAnswerOne(1, 10000000));
+*/
+
+//answer two
+console.log('codewars answer 2')
+
+function filterFunc(n) {
+  return `${n}`.split("").map((x, i) => x ** (i+1)).reduce((a, b) => a+b) == n;
+}
+
+function *range(a, b) {
+  for (var i = a; i <= b; ++i) yield i;
+}
+
+function sumDigPowAnswerTwo(a, b) {
+  return Array.from(range(a, b)).filter(filterFunc);
+}
+
+console.log(sumDigPowAnswerTwo(1,10000000))
+
+
+/*Worikings
+
+//calculation to check whether sum of digits (to the power of position) matches original
 
 
 originalNumber = 135
@@ -21,9 +80,8 @@ if (arraySum = originalNumber) {
 }
 
 console.log(augArray)
-*/
 
-/* calculation to take an array of numbers, check each number against array, and insert into new augented array
+//calculation to take an array of numbers, check each number against array, and insert into new augented array
 
 
 originalArray = [1,2,3,4,5,6,7,8,9,77,89,130,140];
@@ -52,7 +110,7 @@ for (i = 0; i < originalArray.length; i++) {
 console.log(augArray);
 
 
-
+//completed formula
 originalArray = [];
 tempArray = [];
 arraySum = [];
@@ -86,58 +144,5 @@ for (ii = 0; ii < originalArray.length; ii++) {
 console.log(augArray);
 
 */
-
-// function solution
-
-function sumDigPow(a, b) {
-  var originalArray = [];
-	for (var i = a; i < b + 1; i++) {
-	    originalArray.push(i);
-    } 
-    var augArray = [];
-    for (var ii = 0; ii < originalArray.length; ii++) {
-	    var tempArray = originalArray[ii].toString().split("");
-	    var arraySum = 0;
-	    for (var iii = 0; iii < tempArray.length; iii++) {
-		    arraySum = arraySum + Math.pow(tempArray[iii],iii+1);
-		}
-	    if (arraySum == originalArray[ii]) {
-		    augArray.push(originalArray[ii]);
-        }
-    }
-    return augArray
-}
-
-
-
-console.log(sum_dig_pow(1, 10));
-console.log(sum_dig_pow(1, 100));
-console.log(sum_dig_pow(10, 89));
-console.log(sum_dig_pow(10, 100));
-console.log(sum_dig_pow(90, 100));
-console.log(sum_dig_pow(89, 135));
-
-
-//  code academy bullshit
-
-def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
-	originalArray = [];
-	for (i = a; i < b + 1; i++) {
-	    originalArray.push(i);
-    } 
-    augArray = [];
-    for (ii = 0; ii < originalArray.length; ii++) {
-	    tempArray = originalArray[ii].toString().split("");
-	    arraySum = 0;
-	    for (iii = 0; iii < tempArray.length; iii++) {
-		    arraySum = arraySum + Math.pow(tempArray[iii],iii+1);
-		}
-	    if (arraySum == originalArray[ii]) {
-		    augArray.push(originalArray[ii]);
-        }
-    }
-    return augArray
-    
-function 
 
 
