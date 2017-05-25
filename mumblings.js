cec7eaaa0f0b1a8dc0036d92fc9fd7b395dfaeba
russell88brown <russell88brown@gmail.com>
2017-05-25 11:24:46 +0800

    changed second for loop variable

--- mumblings.js
+++ mumblings.js
@@ -9,18 +9,19 @@ accum("cwAt");    // "C-Ww-Aaa-Tttt"
 var newString
 
 function accum(inputString) {
-	newString = ''
-	stringLength = inputString.length;
-	for ( i = 0; i < stringLength ; i++) {
-		if (i != 0) {newString = newString + '-'};
-		for (ii = 0; ii < i+1; ii++) {
-		if (ii = 0) { 
-			newString = newString + inputString.substring(i,i+1).toUpperCase();
-		} else {
-			newString = newString + inputString.substring(i,i+1)}.toLowerCase();
-		} ;
-	}
-	return newString;
+  newString = ''
+  stringLength = inputString.length;
+  for ( i = 0; i < stringLength ; i++) {
+    if (i != 0) {newString = newString + '-'};
+    for ( j = 0; j < 1 + i ; j++) {
+    if (j = 0) { 
+        newString = newString + inputString.substring(i,i+1).toUpperCase();
+    } else {
+        newString = newString + inputString.substring(i,i+1).toLowerCase();
+      }
+    } 
+  }
+  return newString;
 }
 
 console.log(accum("abcd"));
