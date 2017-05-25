49bcd85265e7abe6c8c4e52148e97d8b3800af6a
russell88brown <russell88brown@gmail.com>
2017-05-25 11:40:22 +0800

    fixing merge

--- mumblings.js
+++ mumblings.js
@@ -9,10 +9,6 @@ accum("cwAt");    // "C-Ww-Aaa-Tttt"
 var newString
 
 function accum(inputString) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 85c95f28a0d054cb7a9c2b8e336be1df378c85f2
   newString = ''
   stringLength = inputString.length;
   for ( i = 0; i < stringLength ; i++) {
@@ -23,26 +19,9 @@ function accum(inputString) {
     } else {
         newString = newString + inputString.substring(i,i+1).toLowerCase();
       }
-    } 
+    }
   }
   return newString;
-<<<<<<< HEAD
-=======
-	newString = ''
-	stringLength = inputString.length;
-	for ( i = 0; i < stringLength ; i++) {
-		if (i != 0) {newString = newString + '-'};
-		for (ii = 0; ii < i+1; ii++) {
-		if (ii = 0) { 
-			newString = newString + inputString.substring(i,i+1).toUpperCase();
-		} else {
-			newString = newString + inputString.substring(i,i+1).toLowerCase();
-		} ;
-	}
-	return newString;
->>>>>>> 33a3d1a92cf3736727825ce7fd14b700a39854ee
-=======
->>>>>>> 85c95f28a0d054cb7a9c2b8e336be1df378c85f2
 }
 
 console.log(accum("abcd"));
