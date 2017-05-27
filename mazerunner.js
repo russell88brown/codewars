7d5cf5b34712efa3881635b033b4cad18d472c14
russell88brown <russell88brown@gmail.com>
2017-05-27 19:13:22 +0800

    adding old files

--- mazerunner.js
+++ mazerunner.js
@@ -4,18 +4,33 @@ https://www.codewars.com/kata/maze-runner/
 
 */
 
+//dictionary for movement
 
-function checkCoords(coord) {
+
+var movementArray = {'N': [0,1], 'S': [0.-1], E: [1,0], W: [-1,0]}
+var move = []
+function getMove(coord) {
+	console.log(coord)
+	console.log(movementArray.N)
+	return movementArray.coord;
 }
+
+
 
 function mazeRunner(maze, directions) {
+	var move = getMove('N')
+	console.log(move)
 }
 
+mazeRunner(1,1)
+/*
 mazeRunner(maze, 'N')
 mazeRunner(maze, 'S')
 mazeRunner(maze, 'E')
 mazeRunner(maze, 'W')
 mazeRunner(maze, 'F')
+
+
 
 
 //This is the maze
@@ -27,15 +42,9 @@ var maze = [[1,1,1,1,1,1,1],
             [1,0,0,0,0,0,1],
             [1,2,1,0,1,0,1]];
 
-//this is the movement array
-var movementArray = {
-	'N':[0,1],
-	'S':[0.-1],
-	'E':[1,0],
-	'W':[-1,0],
-}
 
-/*
+
+
 mazeRunner(maze,["N","N","N","N","N","E","E","E","E","E"])
 mazeRunner(maze,["N","N","N","N","N","E","E","S","S","E","E","N","N","E"])
 */
---
+++ sumthreefive.js
@@ -0,0 +1,12 @@
+/*
+sumthreefive.js
+
+https://www.codewars.com/kata/sum-of-all-the-multiples-of-3-or-5/train/javascript
+*/
+
+function findSum(n) {
+	for (index = 0; index < n; index++)
+	console.log(index);
+	}
+console.log(findSum(5));
+console.log(findSum(10));
