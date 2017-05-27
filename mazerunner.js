f07f64633c630197dbd31cd204cf1922101bda32
russell88brown <russell88brown@gmail.com>
2017-05-27 20:15:55 +0800

    created dictionary for movement n,s,e,w

--- mazerunner.js
+++ mazerunner.js
@@ -5,33 +5,25 @@ https://www.codewars.com/kata/maze-runner/
 */
 
 //dictionary for movement
+var movementArray = {N:[0,1], S:[0,-1], E:[1,0], W:[-1,0]};
 
-
-var movementArray = {'N': [0,1], 'S': [0.-1], E: [1,0], W: [-1,0]}
-var move = []
-function getMove(coord) {
-	console.log(coord)
-	console.log(movementArray.N)
-	return movementArray.coord;
+function mazeRunner(a) {
+      var index = movementArray[a];
+      console.log(index);
 }
 
 
+value = 'N';
+mazeRunner(value);
+value = 'S';
+mazeRunner(value);
+value = 'E'
+mazeRunner(value);
+value = 'W'
+mazeRunner(value);
 
-function mazeRunner(maze, directions) {
-	var move = getMove('N')
-	console.log(move)
-}
 
-mazeRunner(1,1)
 /*
-mazeRunner(maze, 'N')
-mazeRunner(maze, 'S')
-mazeRunner(maze, 'E')
-mazeRunner(maze, 'W')
-mazeRunner(maze, 'F')
-
-
-
 
 //This is the maze
 var maze = [[1,1,1,1,1,1,1],
